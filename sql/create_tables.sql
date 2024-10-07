4119e2d980807e2a926d14cfdab804834dd3c1b3
Downzin <downmestre@gmail.com>
2024-10-07 09:54:53 -0300

    :card_file_box: Banco de dados SQL LunaryRoll

---
+++ sql/create_tables.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE animes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    imagem VARCHAR(255)
+);
+    
---
+++ sql/seed_data.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO animes (nome, descricao, imagem) VALUES
+('Naruto', 'Um ninja que sonha em ser Hokage', 'naruto.jpg'),
+('One Piece', 'Piratas em busca do One Piece', 'onepiece.jpg');
+    
